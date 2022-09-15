f9351a622cb3786fd3cdc0a5c7e1da77275ca075
Yusuf Pradnya Darpita <pradnyadarpita@gmail.com>
2022-09-15 23:22:09 +0700

    Repairing the code of left join and right join

--- left_join.sql
+++ left_join.sql
@@ -6,7 +6,7 @@ SELECT
   departments.name
 -- Get the value from employees table within employee_data dataset
 FROM employee_data.employees
--- Using inner join to departments table within employee_data dataset to get the result, with key matching value is department_id from both table
-INNER JOIN 
+-- Using left join to departments table within employee_data dataset to get the result, with key matching value is department_id from both table
+LEFT JOIN 
   employee_data.departments 
   ON employees.department_id = departments.department_id
--- right_join.sql
+++ right_join.sql
@@ -1,4 +1,4 @@
--- LEFT JOIN
+-- RIGHT JOIN
 -- Selecting employee name with alias employee_name, employee role with alias employee_role, and employee's department
 SELECT 
   employees.name AS employee_name,
@@ -6,7 +6,7 @@ SELECT
   departments.name
 -- Get the value from employees table within employee_data dataset
 FROM employee_data.employees
--- Using left join to departments table within employee_data dataset to get the result, with key matching value is department_id from both table
-LEFT JOIN 
+-- Using right join to departments table within employee_data dataset to get the result, with key matching value is department_id from both table
+RIGHT JOIN 
   employee_data.departments 
   ON employees.department_id = departments.department_id
