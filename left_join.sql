e5d6c3e41117dd13e3292cd183c388e592383763
Yusuf Pradnya Darpita <pradnyadarpita@gmail.com>
2022-09-15 23:20:18 +0700

    Add example of sql join

---
+++ left_join.sql
@@ -0,0 +1,12 @@
+-- LEFT JOIN
+-- Selecting employee name with alias employee_name, employee role with alias employee_role, and employee's department
+SELECT 
+  employees.name AS employee_name,
+  employees.role AS employee_role,
+  departments.name
+-- Get the value from employees table within employee_data dataset
+FROM employee_data.employees
+-- Using inner join to departments table within employee_data dataset to get the result, with key matching value is department_id from both table
+INNER JOIN 
+  employee_data.departments 
+  ON employees.department_id = departments.department_id
